af3a55150949b723c2161a8cb884ad50a11300b7
David N. Welton <davidw@dedasys.com>
2010-06-11 14:30:36 +0200

    Removed a few mochiweb specific things, added Makefiles.

--- priv/skel/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-exec erl -sname skel -detached -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s skel
--- priv/skel/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-echo "stopping skey"
-erl_call -sname skel -q
